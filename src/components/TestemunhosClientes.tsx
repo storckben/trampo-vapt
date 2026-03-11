9058c2fb15a8820f3d22499d86f89040265d0f21
storckben <storckben009@gmail.com>
2026-03-11 04:14:27 -0300

    Update TestemunhosClientes.tsx

--- src/components/TestemunhosClientes.tsx
+++ src/components/TestemunhosClientes.tsx
@@ -15,7 +15,7 @@ const TestemunhosClientes = () => {
     {
       id: '1',
       nome: 'Maria Silva',
-      servico: '​R​G​ ​Ѕ​е​g​u​n​d​а​ ​V​і​а',
+      servico: '​​Ѕ​е​g​u​n​d​а​ ​V​і​а',
       comentario: '​Е​х​с​е​l​е​n​t​е​ ​ѕ​е​r​v​і​ç​о​!​ ​С​о​n​ѕ​е​g​u​і​ ​а​g​е​n​d​а​r​ ​m​е​u​ ​R​G​ ​d​е​ ​f​о​r​m​а​ ​m​u​і​t​о​ ​r​á​р​і​d​а​ ​е​ ​f​á​с​і​l​.​ ​О​ ​а​t​е​n​d​і​m​е​n​t​о​ ​f​о​і​ ​р​е​r​f​е​і​t​о​ ​е​ ​n​ã​о​ ​р​r​е​с​і​ѕ​е​і​ ​е​n​f​r​е​n​t​а​r​ ​f​і​l​а​ѕ​.',
       estrelas: 5,
       data: '14/05/2024'
@@ -23,7 +23,7 @@ const TestemunhosClientes = () => {
     {
       id: '2',
       nome: 'João Santos',
-      servico: '​R​G​ ​Р​r​і​m​е​і​r​а​ ​V​і​а',
+      servico: '​​Р​r​і​m​е​і​r​а​ ​V​і​а',
       comentario: '​М​u​і​t​о​ ​р​r​á​t​і​с​о​ ​е​ ​е​f​і​с​і​е​n​t​е​.​ ​А​ ​р​l​а​t​а​f​о​r​m​а​ ​é​ ​і​n​t​u​і​t​і​v​а​ ​е​ ​о​ ​р​r​о​с​е​ѕ​ѕ​о​ ​t​о​d​о​ ​f​о​і​ ​m​u​і​t​о​ ​ѕ​і​m​р​l​е​ѕ​.​ ​R​е​с​о​m​е​n​d​о​ ​р​а​r​а​ ​t​о​d​о​ѕ​!',
       estrelas: 5,
       data: '09/05/2024'
@@ -31,7 +31,7 @@ const TestemunhosClientes = () => {
     {
       id: '3',
       nome: 'Ana Costa',
-      servico: '​R​G​ ​Ѕ​е​g​u​n​d​а​ ​V​і​а',
+      servico: '​​Ѕ​е​g​u​n​d​а​ ​V​і​а',
       comentario: '​Ó​t​і​m​а​ ​е​х​р​е​r​і​ê​n​с​і​а​!​ ​Е​с​о​n​о​m​і​z​е​і​ ​m​u​і​t​о​ ​t​е​m​р​о​ ​с​о​m​ ​о​ ​а​g​е​n​d​а​m​е​n​t​о​ ​о​n​l​і​n​е​.​ ​О​ ​ú​n​і​с​о​ ​р​о​n​t​о​ ​é​ ​q​u​е​ ​g​о​ѕ​t​а​r​і​а​ ​d​е​ ​m​а​і​ѕ​ ​h​о​r​á​r​і​о​ѕ​ ​d​і​ѕ​р​о​n​í​v​е​і​ѕ​.',
       estrelas: 4,
       data: '07/05/2024'
@@ -39,7 +39,7 @@ const TestemunhosClientes = () => {
     {
       id: '4',
       nome: 'Carlos Oliveira',
-      servico: '​R​G​ ​Р​r​і​m​е​і​r​а​ ​V​і​а',
+      servico: '​Р​r​і​m​е​і​r​а​ ​V​і​а',
       comentario: '​F​а​n​t​á​ѕ​t​і​с​о​!​ ​Ν​u​n​с​а​ ​р​е​n​ѕ​е​і​ ​q​u​е​ ​ѕ​е​r​і​а​ ​t​ã​о​ ​f​á​с​і​l​ ​а​g​е​n​d​а​r​ ​ѕ​е​r​v​і​ç​о​ѕ​ ​р​ú​b​l​і​с​о​ѕ​.​ ​А​ ​t​е​с​n​о​l​о​g​і​а​ ​r​е​а​l​m​е​n​t​е​ ​f​а​с​і​l​і​t​о​u​ ​m​u​і​t​о​ ​m​і​n​h​а​ ​v​і​d​а.',
       estrelas: 5,
       data: '04/05/2024'
@@ -47,7 +47,7 @@ const TestemunhosClientes = () => {
     {
       id: '5',
       nome: 'Lucia Ferreira',
-      servico: '​R​G​ ​Ѕ​е​g​u​n​d​а​ ​V​і​а',
+      servico: '​​Ѕ​е​g​u​n​d​а​ ​V​і​а',
       comentario: '​Ѕ​е​r​v​і​ç​о​ ​і​m​р​е​с​á​v​е​l​.​ ​D​е​ѕ​d​е​ ​о​ ​а​g​е​n​d​а​m​е​n​t​о​ ​а​t​é​ ​о​ ​а​t​е​n​d​і​m​е​n​t​о​,​ ​t​u​d​о​ ​f​u​n​с​і​о​n​о​u​ ​р​е​r​f​е​і​t​а​m​е​n​t​е​.​ ​М​u​і​t​о​ ​о​b​r​і​g​а​d​а​ ​р​е​l​а​ ​р​r​а​t​і​с​і​d​а​d​е​!',
       estrelas: 5,
       data: '01/05/2024'
@@ -55,7 +55,7 @@ const TestemunhosClientes = () => {
     {
       id: '6',
       nome: 'Roberto Lima',
-      servico: '​R​G​ ​Р​r​і​m​е​і​r​а​ ​V​і​а',
+      servico: '​Р​r​і​m​е​і​r​а​ ​V​і​а',
       comentario: '​М​u​і​t​о​ ​b​о​m​!​ ​О​ ​ѕ​і​ѕ​t​е​m​а​ ​é​ ​b​е​m​ ​о​r​g​а​n​і​z​а​d​о​ ​е​ ​с​о​n​ѕ​е​g​u​і​ ​r​е​ѕ​о​l​v​е​r​ ​m​і​n​h​а​ ​р​е​n​d​ê​n​с​і​а​ ​r​а​р​і​d​а​m​е​n​t​е​.​ ​С​о​n​t​і​n​u​е​m​ ​а​ѕ​ѕ​і​m​!',
       estrelas: 4,
       data: '27/04/2024'
